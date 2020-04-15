ead57803f2d49ecbf4b4db25c19835c86dc0aa1d
liyaSileshi <liyasileshi@gmail.com>
2020-04-15 12:18:57 -0700

    isogram

---
+++ isogram.py
@@ -0,0 +1,33 @@
+# Determine if a word or phrase is an isogram.
+# An isogram (also known as a "nonpattern word") 
+# is a word or phrase without a repeating letter, 
+# however spaces and hyphens are allowed to appear 
+# multiple times.
+# Examples of isograms(inputs):
+#1, Good inputs
+    # lumberjacks
+    # background
+    # downstream
+    # six-year-old
+
+#make a histogram
+#if key is letter and value is > 1: return False
+def isIsogram(word):
+    hist = dict()
+    #create a histogram
+    for i in word:
+        if i in hist:
+            hist[i] += 1
+        else:
+            hist[i] = 1
+    print(hist)
+    #loop through the histogram keys and values
+    #check if it's a letter and if value == 1
+    for key, value in hist.items():
+        if key.isalpha() and value > 1:
+            return False
+    return True
+
+
+
+print(isIsogram('lumbe-rjac-ks'))
