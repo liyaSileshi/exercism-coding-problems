253e0f74f49589178c7bc232e239ed4d875f4f76
liyaSileshi <liyasileshi@gmail.com>
2020-04-19 19:47:09 -0700

    works

--- wordy.py
+++ wordy.py
@@ -1,24 +1,24 @@
 #Parse and evaluate simple math word problems returning the answer as an integer.
 
-# Iteration 0 — Numbers
 # Problems with no operations simply evaluate to the number given.
 
-# What is 5?
-# Evaluates to 5.
 
-# Add two numbers together.
-# What is 5 plus 13?
-# Evaluates to 18.
-# Handle large numbers and negative numbers.
+#1, Good inputs
+    #What is 5?
+    #What is 5 plus 13?
+    # What is 7 minus 5?
+    # What is 6 multiplied by 4?
+    # What is 25 divided by 5?
 
-# Iteration 2 — Subtraction, Multiplication and Division
-# Now, perform the other three operations.
-# What is 7 minus 5?
-# 2
-# What is 6 multiplied by 4?
-# 24
-# What is 25 divided by 5?
-# 5
+#2, Bad inputs
+    #what is my name?
+    #What is 5 plus plus?
+    # What is 7 and 5?
+
+#3, Edge case inputs
+    # '' - empty string
+    #What is 5?
+
 
 def wordyOperation(string):
     #remove question mark
@@ -47,4 +47,5 @@ def wordyOperation(string):
         if arr[1] + ' ' + arr[2] == 'divided by':
              return int(arr[0]) / int(arr[3])
 
-print(wordyOperation('What is 25 divided by 5?'))
+print(wordyOperation('What is 5 plus 5?'))
+print(wordyOperation(' '))
